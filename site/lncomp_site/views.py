5b12c141826cf6c492ce04c6e52db616ad6d15c3
ndiougand20-commits <ndiougand20@gmail.com>
2025-11-10 16:10:53 +0000

    modif

--- site/lncomp_site/views.py
+++ site/lncomp_site/views.py
@@ -1,18 +1,2 @@
 from django.shortcuts import render
 from main.models import About, Service, Realisation, Lifestyle, MembreEquipe
-
-def index(request):
-    about = About.objects.first()
-    services = Service.objects.all().order_by('ordre')
-    realisations = Realisation.objects.all()[:6]  # latest 6
-    lifestyles = Lifestyle.objects.all()[:6]  # latest 6
-    equipe = MembreEquipe.objects.all().order_by('ordre')
-
-    context = {
-        'about': about,
-        'services': services,
-        'realisations': realisations,
-        'lifestyles': lifestyles,
-        'equipe': equipe,
-    }
-    return render(request, 'index.html', context)
